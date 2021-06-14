01ae8e6fa178a5e645d13d6272583b7b705a0ec9
Alexia Danan <dananalexia@gmail.com>
2021-06-15 01:05:21 +0200

    cafe controller

--- app/controllers/cafes_controller.rb
+++ app/controllers/cafes_controller.rb
@@ -1,46 +1,63 @@
 class CafesController < ApplicationController
-
+  before_action :skip_pundit?
   before_action :set_cafe, only: [:show, :edit, :update, :destroy]
 
   def index
-    @cafes = Cafe.all
+    @cafes = policy_scope(Cafe)
+
+
+    @markers = @cafes.geocoded.map do |cafe|
+      {
+      lat: cafe.latitude,
+      lng: cafe.longitude,
+      # info_window: render_to_string(partial: "info_window", locals: { icon: icon }), #optional
+      # image_url: helpers.asset_url('REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS, ie the URL in cloudinary for instance') #optional
+      }
+
+  end
 
   end
 
 
   def show
+    @tables = @cafe.tables
+
+
+    authorize @cafe
+    authorize @tables
+
   end
 
   def new
-
     @cafe = Cafe.new
-
   end
 
   def create
     @cafe = Cafe.new(cafe_params)
+    @cafe.user = current_user
 
     if @cafe.save
       redirect_to @cafe, notice: "Your Cafe has been created!"
     else
       render :new
+    end
   end
 
   def edit
-
+    authorize @cafe
   end
 
   def update
     if @cafe.update(cafe_params)
-      redirect_to @cafe. notice: "Your Cafe has been updated"
+      redirect_to @cafe, notice: "Your Cafe has been updated"
     else
     render :edit
    end
-
-
   end
 
   def destroy
+    @cafe.destroy
+    redirect_to cafes_path, notice: "Your Cafe was succesfully destroyed"
   end
 
   private
@@ -51,7 +68,7 @@ class CafesController < ApplicationController
 
   def cafe_params
 
-    params.require(:cafe).permit(:name, :location, :photo)
+  params.require(:cafe).permit(:name, :location, :photo)
 
   end
 
---
+++ app/javascript/plugins/init_mapbox.js
@@ -0,0 +1,59 @@
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+
+const buildMap = (mapElement) => {
+  mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+  return new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10'
+    //choose your style   or design your own
+  });
+};
+
+const addMarkersToMap = (map, markers) => {
+  markers.forEach((marker) => {
+    const popup=new mapboxgl.Popup().setHTML(marker.info_window);
+
+    // Create a HTML element for your custom marker
+    // const element= document.createElement('div');
+    // element.className= 'marker';
+    // element.style.backgroundImage= `url('${marker.image_url}')`;
+    // element.style.backgroundSize= 'contain';
+    // element.style.width= '25px';
+    // element.style.height= '25px';
+
+    new mapboxgl.Marker() //only pass the element variable if you styled your markers(const above)- otherwise keep blank
+      .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup)
+      .addTo(map);
+  });
+};
+
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, {
+    padding: 70,
+    maxZoom: 15,
+    duration: 2800
+  });
+};
+
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
+  if (mapElement) { // only build a map if there's a div#map to inject into
+    const map = buildMap(mapElement);
+    const markers = JSON.parse(mapElement.dataset.markers);
+    //only add if you want search bar:
+    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken, mapboxgl: mapboxgl }));
+    map.addControl(new mapboxgl.GeolocateControl({ positionOptions: {enableHighAccuracy: true}, trackUserLocation: true}));
+    // call your functions:
+    addMarkersToMap(map, markers);
+    fitMapToMarkers(map, markers);
+  }
+};
+
+export { initMapbox };
