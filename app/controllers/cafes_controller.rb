4fe8d2f6aa4f978d5365e3415858713d794670eb
Alexia Danan <dananalexia@gmail.com>
2021-06-14 16:42:41 +0200

    added cloudinary

--- app/controllers/cafes_controller.rb
+++ app/controllers/cafes_controller.rb
@@ -1,2 +1,35 @@
 class CafesController < ApplicationController
+  def index
+  end
+
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_cafe
+    @cafe = Cafe.find(params[:id])
+  end
+
+  def cafe_params
+
+  end
+
+
 end
