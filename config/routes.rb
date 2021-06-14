6b11b1bf2722b34ee6c76646c87c0af76aa0e5ef
Alexia Danan <dananalexia@gmail.com>
2021-06-14 15:10:57 +0200

    initialisze

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cafes do
-    resources :bookings, only: [:new, :create]
-    end
+  resources :cafes
 
-  resources :tables, only: [:new, :create, :edit, :update, :destroy]
-  resources :bookings, only: [:show, :edit, :update, :destroy, :confirm] do
+
+  resources :tables, only: [] do
+    resources :bookings, only: [:create]
+  end
+
+  resources :bookings, only: [:show, :edit, :update, :destroy] do
     resources :reviews, only: [:new, :create]
   end
 
