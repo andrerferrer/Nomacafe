0690539f1515afbcaedcea4402a76e978fe61591
millad shadpour <shadpourmillad@gmail.com>
2021-06-17 14:28:35 +0200

    added color change for list view

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,7 @@ class Booking < ApplicationRecord
   belongs_to :table
   has_one :review, dependent: :nullify
   has_one :cafe, through: :table
+  enum status: [:pending, :accepted, :declined]
 
   def booking_headline
     "You have a #{status} seat at #{table.cafe.name} located at #{table.cafe.location} you have a #{table.name} #{table.seat} costing €#{((table.min_credits * (end_time - start_time)) / 7200)}"
